1c9d5883aff651db9c1760e98493658aaf15c0b1
Carmila Angela Reano <s2023101804@firstasia.edu.ph>
2024-11-24 14:59:47 +0800

    aboutinfo

--- ITECC_OrderSystem/ITECC_OrderSystem/Form1.Designer.vb
+++ ITECC_OrderSystem/ITECC_OrderSystem/Form1.Designer.vb
@@ -22,70 +22,339 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        bttn_appetizers = New Button()
-        Panel1 = New Panel()
-        bttn_mainCourse = New Button()
+        components = New ComponentModel.Container()
         Label1 = New Label()
+        AboutInfo = New Panel()
+        Panel3 = New Panel()
+        Label_brojan3 = New Label()
+        Label_reano2 = New Label()
+        Label_tauries3 = New Label()
+        Label_dc3 = New Label()
+        Label_brojan2 = New Label()
+        Label_reano3 = New Label()
+        Label_tauries2 = New Label()
+        Label_dc2 = New Label()
+        Label_reano1 = New Label()
+        Label_tauries1 = New Label()
+        PictureBox4 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label_dc1 = New Label()
+        PictureBox1 = New PictureBox()
+        Label_brojan1 = New Label()
+        Brojan = New PictureBox()
+        Panel1 = New Panel()
+        CollapseAILogo = New PictureBox()
+        Label_AboutInfo = New Label()
+        TimerPanel_Collapse = New Timer(components)
+        TimerPanel_Extend = New Timer(components)
+        AboutInfo.SuspendLayout()
+        Panel3.SuspendLayout()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Brojan, ComponentModel.ISupportInitialize).BeginInit()
         Panel1.SuspendLayout()
+        CType(CollapseAILogo, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
-        ' 
-        ' bttn_appetizers
-        ' 
-        bttn_appetizers.Location = New Point(75, 19)
-        bttn_appetizers.Name = "bttn_appetizers"
-        bttn_appetizers.Size = New Size(112, 29)
-        bttn_appetizers.TabIndex = 0
-        bttn_appetizers.Text = "Appetizers"
-        bttn_appetizers.UseVisualStyleBackColor = True
-        ' 
-        ' Panel1
-        ' 
-        Panel1.BackColor = Color.AntiqueWhite
-        Panel1.Controls.Add(bttn_mainCourse)
-        Panel1.Controls.Add(bttn_appetizers)
-        Panel1.Dock = DockStyle.Bottom
-        Panel1.Location = New Point(0, 463)
-        Panel1.Name = "Panel1"
-        Panel1.Size = New Size(1027, 76)
-        Panel1.TabIndex = 1
-        ' 
-        ' bttn_mainCourse
-        ' 
-        bttn_mainCourse.Location = New Point(193, 19)
-        bttn_mainCourse.Name = "bttn_mainCourse"
-        bttn_mainCourse.Size = New Size(112, 29)
-        bttn_mainCourse.TabIndex = 1
-        bttn_mainCourse.Text = "Main Course"
-        bttn_mainCourse.UseVisualStyleBackColor = True
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Font = New Font("Stardew Valley Stonks", 24F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Font = New Font("Stardew Valley Stonks", 24.0F, FontStyle.Bold, GraphicsUnit.Point)
         Label1.Location = New Point(330, 30)
         Label1.Name = "Label1"
         Label1.Size = New Size(339, 43)
         Label1.TabIndex = 2
         Label1.Text = "Ordering System"
         ' 
+        ' AboutInfo
+        ' 
+        AboutInfo.BackColor = Color.FromArgb(CByte(162), CByte(139), CByte(85))
+        AboutInfo.Controls.Add(Panel3)
+        AboutInfo.Controls.Add(Panel1)
+        AboutInfo.Dock = DockStyle.Left
+        AboutInfo.Location = New Point(0, 0)
+        AboutInfo.Name = "AboutInfo"
+        AboutInfo.Size = New Size(203, 539)
+        AboutInfo.TabIndex = 3
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.FromArgb(CByte(203), CByte(226), CByte(181))
+        Panel3.Controls.Add(Label_AboutInfo)
+        Panel3.Controls.Add(Label_brojan3)
+        Panel3.Controls.Add(Label_reano2)
+        Panel3.Controls.Add(Label_tauries3)
+        Panel3.Controls.Add(Label_dc3)
+        Panel3.Controls.Add(Label_brojan2)
+        Panel3.Controls.Add(Label_reano3)
+        Panel3.Controls.Add(Label_tauries2)
+        Panel3.Controls.Add(Label_dc2)
+        Panel3.Controls.Add(Label_reano1)
+        Panel3.Controls.Add(Label_tauries1)
+        Panel3.Controls.Add(PictureBox4)
+        Panel3.Controls.Add(PictureBox3)
+        Panel3.Controls.Add(PictureBox2)
+        Panel3.Controls.Add(Label_dc1)
+        Panel3.Controls.Add(PictureBox1)
+        Panel3.Controls.Add(Label_brojan1)
+        Panel3.Controls.Add(Brojan)
+        Panel3.Dock = DockStyle.Top
+        Panel3.Location = New Point(0, 37)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(203, 502)
+        Panel3.TabIndex = 7
+        ' 
+        ' Label_brojan3
+        ' 
+        Label_brojan3.AutoSize = True
+        Label_brojan3.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label_brojan3.Location = New Point(99, 137)
+        Label_brojan3.Name = "Label_brojan3"
+        Label_brojan3.Size = New Size(67, 18)
+        Label_brojan3.TabIndex = 16
+        Label_brojan3.Text = "Database"
+        ' 
+        ' Label_reano2
+        ' 
+        Label_reano2.AutoSize = True
+        Label_reano2.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_reano2.Location = New Point(108, 444)
+        Label_reano2.Name = "Label_reano2"
+        Label_reano2.Size = New Size(50, 17)
+        Label_reano2.TabIndex = 15
+        Label_reano2.Text = "Reano"
+        ' 
+        ' Label_tauries3
+        ' 
+        Label_tauries3.AutoSize = True
+        Label_tauries3.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label_tauries3.Location = New Point(121, 362)
+        Label_tauries3.Name = "Label_tauries3"
+        Label_tauries3.Size = New Size(47, 18)
+        Label_tauries3.TabIndex = 14
+        Label_tauries3.Text = "Assets"
+        ' 
+        ' Label_dc3
+        ' 
+        Label_dc3.AutoSize = True
+        Label_dc3.Font = New Font("Times New Roman", 10.2F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label_dc3.Location = New Point(89, 245)
+        Label_dc3.Name = "Label_dc3"
+        Label_dc3.Size = New Size(100, 20)
+        Label_dc3.TabIndex = 13
+        Label_dc3.Text = "Programmer"
+        ' 
+        ' Label_brojan2
+        ' 
+        Label_brojan2.AutoSize = True
+        Label_brojan2.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_brojan2.Location = New Point(107, 120)
+        Label_brojan2.Name = "Label_brojan2"
+        Label_brojan2.Size = New Size(52, 17)
+        Label_brojan2.TabIndex = 12
+        Label_brojan2.Text = "Brojan"
+        ' 
+        ' Label_reano3
+        ' 
+        Label_reano3.AutoSize = True
+        Label_reano3.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label_reano3.Location = New Point(103, 461)
+        Label_reano3.Name = "Label_reano3"
+        Label_reano3.Size = New Size(65, 18)
+        Label_reano3.TabIndex = 11
+        Label_reano3.Text = "Designer"
+        ' 
+        ' Label_tauries2
+        ' 
+        Label_tauries2.AutoSize = True
+        Label_tauries2.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_tauries2.Location = New Point(114, 348)
+        Label_tauries2.Name = "Label_tauries2"
+        Label_tauries2.Size = New Size(64, 17)
+        Label_tauries2.TabIndex = 10
+        Label_tauries2.Text = "Maestre"
+        ' 
+        ' Label_dc2
+        ' 
+        Label_dc2.AutoSize = True
+        Label_dc2.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_dc2.Location = New Point(99, 228)
+        Label_dc2.Name = "Label_dc2"
+        Label_dc2.Size = New Size(79, 17)
+        Label_dc2.TabIndex = 9
+        Label_dc2.Text = "De Chavez"
+        ' 
+        ' Label_reano1
+        ' 
+        Label_reano1.AutoSize = True
+        Label_reano1.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_reano1.Location = New Point(89, 427)
+        Label_reano1.Name = "Label_reano1"
+        Label_reano1.Size = New Size(108, 17)
+        Label_reano1.TabIndex = 8
+        Label_reano1.Text = "Carmila Angela"
+        ' 
+        ' Label_tauries1
+        ' 
+        Label_tauries1.AutoSize = True
+        Label_tauries1.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_tauries1.Location = New Point(77, 321)
+        Label_tauries1.Name = "Label_tauries1"
+        Label_tauries1.Size = New Size(126, 17)
+        Label_tauries1.TabIndex = 7
+        Label_tauries1.Text = "Tauries Angellyka"
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.BackgroundImage = My.Resources.Resources.IMG_2893
+        PictureBox4.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox4.Location = New Point(12, 293)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(67, 87)
+        PictureBox4.TabIndex = 6
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackgroundImage = My.Resources.Resources.IMG_1866
+        PictureBox3.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox3.Location = New Point(12, 403)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(67, 87)
+        PictureBox3.TabIndex = 5
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Location = New Point(41, 303)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(0, 0)
+        PictureBox2.TabIndex = 4
+        PictureBox2.TabStop = False
+        ' 
+        ' Label_dc1
+        ' 
+        Label_dc1.AutoSize = True
+        Label_dc1.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_dc1.Location = New Point(99, 211)
+        Label_dc1.Name = "Label_dc1"
+        Label_dc1.Size = New Size(84, 17)
+        Label_dc1.TabIndex = 3
+        Label_dc1.Text = "Jann Railey"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImage = My.Resources.Resources.IMG_2084
+        PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox1.Location = New Point(12, 178)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(67, 87)
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Label_brojan1
+        ' 
+        Label_brojan1.AutoSize = True
+        Label_brojan1.Font = New Font("Times New Roman", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_brojan1.Location = New Point(103, 103)
+        Label_brojan1.Name = "Label_brojan1"
+        Label_brojan1.Size = New Size(60, 17)
+        Label_brojan1.TabIndex = 1
+        Label_brojan1.Text = "Janmar "
+        ' 
+        ' Brojan
+        ' 
+        Brojan.BackgroundImage = My.Resources.Resources.IMG_2774
+        Brojan.BackgroundImageLayout = ImageLayout.Stretch
+        Brojan.Location = New Point(12, 68)
+        Brojan.Name = "Brojan"
+        Brojan.Size = New Size(67, 87)
+        Brojan.TabIndex = 0
+        Brojan.TabStop = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Cornsilk
+        Panel1.Controls.Add(CollapseAILogo)
+        Panel1.Dock = DockStyle.Top
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(203, 37)
+        Panel1.TabIndex = 5
+        ' 
+        ' CollapseAILogo
+        ' 
+        CollapseAILogo.BackgroundImage = My.Resources.Resources.menu
+        CollapseAILogo.BackgroundImageLayout = ImageLayout.Zoom
+        CollapseAILogo.Dock = DockStyle.Right
+        CollapseAILogo.Location = New Point(174, 0)
+        CollapseAILogo.Name = "CollapseAILogo"
+        CollapseAILogo.Size = New Size(29, 37)
+        CollapseAILogo.TabIndex = 0
+        CollapseAILogo.TabStop = False
+        ' 
+        ' Label_AboutInfo
+        ' 
+        Label_AboutInfo.AutoSize = True
+        Label_AboutInfo.Font = New Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_AboutInfo.Location = New Point(18, 28)
+        Label_AboutInfo.Name = "Label_AboutInfo"
+        Label_AboutInfo.Size = New Size(171, 25)
+        Label_AboutInfo.TabIndex = 17
+        Label_AboutInfo.Text = "Meet the Team!"
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.Cornsilk
         ClientSize = New Size(1027, 539)
+        Controls.Add(AboutInfo)
         Controls.Add(Label1)
-        Controls.Add(Panel1)
+        MaximizeBox = False
+        MinimizeBox = False
         Name = "Form1"
-        Text = "Form1"
+        Text = "Ordering System"
+        AboutInfo.ResumeLayout(False)
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(Brojan, ComponentModel.ISupportInitialize).EndInit()
         Panel1.ResumeLayout(False)
+        CType(CollapseAILogo, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
 
     End Sub
-
-    Friend WithEvents bttn_appetizers As Button
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents bttn_mainCourse As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents AboutInfo As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents CollapseAILogo As PictureBox
+    Friend WithEvents Brojan As PictureBox
+    Friend WithEvents Label_brojan1 As Label
+    Friend WithEvents Label_reano2 As Label
+    Friend WithEvents Label_tauries3 As Label
+    Friend WithEvents Label_dc3 As Label
+    Friend WithEvents Label_brojan2 As Label
+    Friend WithEvents Label_reano3 As Label
+    Friend WithEvents Label_tauries2 As Label
+    Friend WithEvents Label_dc2 As Label
+    Friend WithEvents Label_reano1 As Label
+    Friend WithEvents Label_tauries1 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label_dc1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label_brojan3 As Label
+    Friend WithEvents Label_AboutInfo As Label
+    Friend WithEvents TimerPanel_Collapse As Timer
+    Friend WithEvents TimerPanel_Extend As Timer
 End Class
